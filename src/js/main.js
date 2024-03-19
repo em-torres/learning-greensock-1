54d36f7fb37970834b670febdfc0424795087fa9
Edwin Marte Torres <kdgpro15@gmail.com>
2024-03-19 14:38:40 -0400

    Creating the core functions to be used

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,14 @@ gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(MotionPathPlugin);
 
 window.onload = function () {
+    // Main Starting Point
     gsap.set(".hero_circle, .hero_square, .hero_square-2", {
         autoAlpha: 0,
         transformOrigin: "50% 50%",
         scale: 0
     })
 
+    // Lesson 1 - Timelines and Defaults
     let hero_tl = gsap.timeline({
         defaults: {
             stagger: {amount: 0.2},
@@ -19,4 +21,43 @@ window.onload = function () {
     hero_tl.to(".hero_circle", {})
         .to('.hero_square', {})
         .to('.hero_square-2', {})
+
+    // Lesson 2 - ScrollTrigger
+    gsap.from(".move", {
+        scrollTrigger: {
+            trigger: "#about",
+            start: "top 50%",
+            end: "center 50%",
+            scrub: 1,
+            markers: true
+        },
+        scale: 0,
+        duration: 1,
+    })
+
+    // Lesson 3 - Reusable Functions, Complex Animations and Main Timelines
+    function lineAnimation(line) {
+        let lineTL = gsap.timeline()
+        lineTL.from(line, {
+            stagger: {amount: 0.5},
+            ease: "back.out(1.7)",
+            scaleX: 0,
+            duration: 1
+        })
+
+        return lineTL
+    }
+
+    function paraAnimation(paragraph) {
+        let paraTL = gsap.timeline()
+        paraTL.from(paragraph, {
+            scale: 0,
+            x: -200,
+            duration: 1
+        })
+
+        return paraTL
+    }
+
+    // Setting up a Main Timeline
 }
