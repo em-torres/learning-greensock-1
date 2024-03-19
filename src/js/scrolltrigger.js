446e94507e15cf9bff4230bd09f7ea8a439aaa36
Edwin Marte Torres <kdgpro15@gmail.com>
2024-03-19 14:14:52 -0400

    Adding a Scrub in seconds

--- src/js/scrolltrigger.js
+++ src/js/scrolltrigger.js
@@ -7,7 +7,7 @@ window.onload = function () {
         scrollTrigger: {
             trigger: '.cards',  // The trigger element or the element that will be animated (triggered).
             start: 'top top',   // When the top of the trigger hits the top of the viewport.
-            end: '+=100',       // When the trigger hits 100 pixels beyond the viewport.
+            end: '+=10',       // When the trigger hits 100 pixels beyond the viewport.
             // markers: true,      // Show the markers on the viewport.
             markers: {                  // Array to modify how to show the markers on the viewport.
                 startColor: "#ff771c",
@@ -15,7 +15,9 @@ window.onload = function () {
                 fontSize: "20px",
                 fontWeight: "bold",
                 indent: 20
-            }
+            },
+            scrub: 2,           // The animation will be scrubbed in the viewport (in order to have the effect of doing
+                                // it back and forth).
         },
         stagger: { amount: 0.5 },
         scale: 0,
