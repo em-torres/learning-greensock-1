138b2b3c7c3b0eac5a7af929f31dcdb53e0eb867
Edwin Marte Torres <kdgpro15@gmail.com>
2024-03-19 14:03:50 -0400

    Adding an start position and ending to trigger

--- src/js/scrolltrigger.js
+++ src/js/scrolltrigger.js
@@ -5,9 +5,11 @@ window.onload = function () {
     // -- Class 1
     gsap.from('article', {
         scrollTrigger: {
-            trigger: '.cards',
+            trigger: '.cards',  // The trigger element or the element that will be animated (triggered).
+            start: 'top top',   // When the top of the trigger hits the top of the viewport.
+            end: '+=100',       // When the bottom of the trigger hits the bottom of the viewport.
         },
-        stagger: {amount: 0.5},
+        stagger: { amount: 0.5 },
         scale: 0,
         duration: 1
     })
