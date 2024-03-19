286fcd5f160b9c359dfeb3be8730b08351c154ad
Edwin Marte Torres <kdgpro15@gmail.com>
2024-03-19 14:08:48 -0400

    Adding markers on the scrollTrigger to better debug

--- src/js/scrolltrigger.js
+++ src/js/scrolltrigger.js
@@ -7,7 +7,15 @@ window.onload = function () {
         scrollTrigger: {
             trigger: '.cards',  // The trigger element or the element that will be animated (triggered).
             start: 'top top',   // When the top of the trigger hits the top of the viewport.
-            end: '+=100',       // When the bottom of the trigger hits the bottom of the viewport.
+            end: '+=100',       // When the trigger hits 100 pixels beyond the viewport.
+            // markers: true,      // Show the markers on the viewport.
+            markers: {                  // Array to modify how to show the markers on the viewport.
+                startColor: "#ff771c",
+                endColor: "black",
+                fontSize: "20px",
+                fontWeight: "bold",
+                indent: 20
+            }
         },
         stagger: { amount: 0.5 },
         scale: 0,
