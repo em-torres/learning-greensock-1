569bbe8d3a672e657aa8caf83362a39c847a083c
Edwin Marte Torres <kdgpro15@gmail.com>
2024-03-18 20:55:20 -0400

    Working with timelines

--- src/js/tween.js
+++ src/js/tween.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    // Class 3
+    // -- Class 1
     gsap.to(".container .box2", {
         duration: 2,
         y: "200%",
@@ -7,7 +7,7 @@ window.onload = function() {
         repeat: -1,
     })
 
-    // Class 2
+    // -- Class 2
     gsap.set(".container2 .box2", {
         opacity: 0,
     })
@@ -19,7 +19,7 @@ window.onload = function() {
         repeat: -1,
     })
 
-    // Class 3
+    // -- Class 3
     gsap.to(".container3 .box", {
         duration: 0.5,
         y: "200%",
@@ -35,4 +35,32 @@ window.onload = function() {
             // Takes the position from which the staggering should start. It take a number between 0 and the qty of the array or values "end", "center", "edges", etc.... see documentation.
         }
     })
+
+    // -- Class 4
+    let tl = gsap.timeline()
+    tl.timeScale(2) // Speed up the timeline
+
+    tl.to(".container4 .box1", {
+        duration: 1,
+        scale: 0,
+        opacity: 0,
+    })
+        .to(".container4 .box2", {
+            duration: 1,
+            x: "-100%"
+        })
+        .to(".container4 .box3", {
+            duration: 1,
+            x: "-100%",
+            rotate: 360
+        })
+        .set(".container4 .box1", {
+            x: "225%"
+        })
+        .to(".container4 .box1", {
+            duration: 1,
+            scale: 1,
+            opacity: 1,
+        })
+        .repeat(-1)
 }
