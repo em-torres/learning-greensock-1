c9abc0bd4b58ecd4602ee6147e3fb7db037d62a0
Edwin Marte Torres <kdgpro15@gmail.com>
2024-03-19 14:28:09 -0400

    Adding what was learned on Lesson 1

--- src/js/main.js
+++ src/js/main.js
@@ -8,4 +8,15 @@ window.onload = function () {
         scale: 0
     })
 
+    let hero_tl = gsap.timeline({
+        defaults: {
+            stagger: {amount: 0.2},
+            autoAlpha: 1,
+            scale: 1,
+            ease: "back.out(1.7)"
+        },
+    })
+    hero_tl.to(".hero_circle", {})
+        .to('.hero_square', {})
+        .to('.hero_square-2', {})
 }
